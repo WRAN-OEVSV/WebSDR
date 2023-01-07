5450f768ceeae82ba29358f6846bcf2ad705ac7b
Fabian Franz <franz.fabian.94@gmail.com>
2023-01-07 19:01:10 +0100

    RPX-100.cpp use option parser

--- RPX-100.cpp
+++ RPX-100.cpp
@@ -11,11 +11,75 @@
 
 #include "RPX-100.h"
 #include <pthread.h>
+#include <getopt.h>
 
 using namespace std;
 
+
+void parse_option_port(int& port) {
+    auto port_l = strtol(optarg, nullptr, 10);
+    if (port_l > UINT16_MAX) {
+        cerr << "Port is to large" << endl;
+        exit(1);
+    }
+    if (port_l <= 0) {
+        cerr << "Port is too small" << endl;
+        exit(1);
+    }
+    port = (int) port_l;
+}
+
+void parse_option_mode() {
+    mode = (string)optarg;
+    if (mode == "RX")
+    {
+        cout << "Starting RPX-100 with following setting:\n";
+        cout << "Mode: " << optarg << endl;
+        modeSel = 0;
+    }
+    else if (mode == "TXDirect")
+    {
+        cout << "Starting RPX-100 with following setting:\n";
+        cout << "Mode: " << optarg << endl;
+        modeSel = 1;
+    }
+    else if (mode == "TX6m")
+    {
+        cout << "Starting RPX-100 with following setting:\n";
+        cout << "Mode: " << optarg << endl;
+        modeSel = 2;
+    }
+    else if (mode == "TX2m")
+    {
+        cout << "Starting RPX-100 with following setting:\n";
+        cout << "Mode: " << optarg << endl;
+        modeSel = 3;
+    }
+    else if (mode == "TX70cm")
+    {
+        cout << "Starting RPX-100 with following setting:\n";
+        cout << "Mode: " << optarg << endl;
+        modeSel = 4;
+    }
+}
+
+void parse_option_help() {
+    cout << "Options for starting RPX-100: RF-test \033[36mMODE\033[0m" << endl;
+    cout << endl;
+    cout << "\033[36mMODE\033[0m:" << endl;
+    cout << "     \033[32mRX\033[0m for receive mode" << endl;
+    cout << "     \033[31mTXDirect\033[0m for transmit mode without bandpass filter" << endl;
+    cout << "     \033[31mTX6m\033[0m for transmit mode with bandpass filter for 50-54 MHz" << endl;
+    cout << "     \033[31mTX2m\033[0m for transmit mode with bandpass filter for 144-146 MHz" << endl;
+    cout << "     \033[31mTX70cm\033[0m for transmit mode with bandpass filter for 430-440 MHz" << endl;
+    cout << endl;
+    exit(0);
+}
+
+
 int main(int argc, char *argv[])
 {
+    int port = 8085;
     if (argc == 1)
     {
         cout << "Starting RPX-100 with default settings:\n";
@@ -25,61 +89,35 @@ int main(int argc, char *argv[])
     }
     else if (argc >= 2)
     {
-        for (int c = 0; c < argc; c++)
-        {
-            switch (c)
-            {
-            case 1:
-                mode = (string)argv[c];
-                if (mode == "RX")
-                {
-                    cout << "Starting RPX-100 with following setting:\n";
-                    cout << "Mode: " << argv[c] << endl;
-                    modeSel = 0;
-                }
-                else if (mode == "TXDirect")
-                {
-                    cout << "Starting RPX-100 with following setting:\n";
-                    cout << "Mode: " << argv[c] << endl;
-                    modeSel = 1;
-                }
-                else if (mode == "TX6m")
-                {
-                    cout << "Starting RPX-100 with following setting:\n";
-                    cout << "Mode: " << argv[c] << endl;
-                    modeSel = 2;
-                }
-                else if (mode == "TX2m")
-                {
-                    cout << "Starting RPX-100 with following setting:\n";
-                    cout << "Mode: " << argv[c] << endl;
-                    modeSel = 3;
-                }
-                else if (mode == "TX70cm")
-                {
-                    cout << "Starting RPX-100 with following setting:\n";
-                    cout << "Mode: " << argv[c] << endl;
-                    modeSel = 4;
-                }
-                else if (mode == "help")
-                {
-                    cout << "Options for starting RPX-100: RF-test \033[36mMODE\033[0m" << endl;
-                    cout << endl;
-                    cout << "\033[36mMODE\033[0m:" << endl;
-                    cout << "     \033[32mRX\033[0m for receive mode" << endl;
-                    cout << "     \033[31mTXDirect\033[0m for transmit mode without bandpass filter" << endl;
-                    cout << "     \033[31mTX6m\033[0m for transmit mode with bandpass filter for 50-54 MHz" << endl;
-                    cout << "     \033[31mTX2m\033[0m for transmit mode with bandpass filter for 144-146 MHz" << endl;
-                    cout << "     \033[31mTX70cm\033[0m for transmit mode with bandpass filter for 430-440 MHz" << endl;
-                    cout << endl;
-                    return 0;
-                }
-                else
-                {
-                    cout << "Wrong settings, please type  \033[36m'RF-test help'\033[0m to see all options !" << endl;
-                    return 0;
-                }
+        option longopts[] = {
+            {"mode", required_argument, nullptr, 'm'},
+            {"port", required_argument, nullptr, 'p'},
+            {"help", no_argument, nullptr, 'h'},
+            {nullptr}
+        };
+
+        while (true) {
+            const int opt = getopt_long(argc, argv, "m:p:h", longopts, nullptr);
+
+            if (opt == -1) {
                 break;
+            }
+
+
+            switch (opt)
+            {
+                case 'm':
+                    parse_option_mode();
+                    break;
+                case 'p':
+                    parse_option_port(port);
+                    break;
+                case 'h':
+                    parse_option_help();
+                    break;
+                default:
+                    cout << "Wrong settings, please type  \033[36m'RF-test --help'\033[0m to see all options !" << endl;
+                    exit(1);
             }
         }
     }
